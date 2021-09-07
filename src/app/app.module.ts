7b1568db20f6ce4fe09b2adf6d32a18f34c0dfc0
Runi-Holgersson <runi.holger@gmail.com>
2021-09-07 14:34:12 +0400

    remove core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,34 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {FooterComponent} from './footer/footer.component';
-import {CoreModule} from "./core/core.module";
 import { FormsModule }   from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+
+import {AppComponent} from './app.component';
+import {ContentComponent} from "./content/content.component";
+import {BreadcrumpsComponent} from "./content/breadcrumps/breadcrumps.component";
+import {HeaderComponent} from "./content/header/header.component";
+import {MainComponent} from "./content/main/main.component";
+import {CourseItemComponent} from "./content/main/course-item/course-item.component";
+import {LoaderComponent} from "./content/main/loader/loader.component";
+import {SearchPanelComponent} from "./content/main/search-panel/search-panel.component";
+import {FooterComponent} from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    ContentComponent,
+    BreadcrumpsComponent,
+    HeaderComponent,
+    MainComponent,
+    CourseItemComponent,
+    LoaderComponent,
+    SearchPanelComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule,
     FormsModule
   ],
   providers: [],
