040a6a4f09315589295c7e4f473a04cfafbeb76b
Runi-Holgersson <runi.holger@gmail.com>
2021-09-15 14:26:51 +0400

    apply built-in date pipe

--- src/app/content/main/main.component.ts
+++ src/app/content/main/main.component.ts
@@ -20,15 +20,15 @@ export class MainComponent implements OnInit {
   Ab deserunt, dignissimos dolores eaque eius eligendi.`},
       {title: `Course 2`, duration: 130, date: `07.09.2019`, description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
   Ab deserunt, dignissimos dolores eaque eius eligendi.`, topRated: true},
-      {title: `Course 3`, duration: 140, date: `07.09.2020`, description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      {title: `Course 3`, duration: 140, date: `07 Jun 2020`, description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
   Ab deserunt, dignissimos dolores eaque eius eligendi.`},
       {title: `Course 4`, duration: 150, date: `07.09.2021`, description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
   Ab deserunt, dignissimos dolores eaque eius eligendi.`, topRated: true},
       {title: `Course 2`, duration: 160, date: `07.09.2019`, description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
   Ab deserunt, dignissimos dolores eaque eius eligendi.`, topRated: false},
-      {title: `Course 2`, duration: 180, date: `07.09.2019`, description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      {title: `Course 2`, duration: 180, date: `07 Aug 2019`, description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
   Ab deserunt, dignissimos dolores eaque eius eligendi.`, topRated: true},
-      {title: `Course 2`, duration: 190, date: `07.09.2019`, description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      {title: `Course 2`, duration: 190, date: `07 Sept 2019`, description:`Lorem ipsum dolor sit amet, consectetur adipisicing elit.
   Ab deserunt, dignissimos dolores eaque eius eligendi.`, topRated: true},
     ]
   }
