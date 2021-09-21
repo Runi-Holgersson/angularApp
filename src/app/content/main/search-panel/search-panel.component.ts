f0a387e52ca7d6f6c3fa7dcfa13bd2577c1853da
Runi-Holgersson <runi.holger@gmail.com>
2021-09-21 14:20:40 +0400

    delete ngOnInit in search-panel component

--- src/app/content/main/search-panel/search-panel.component.ts
+++ src/app/content/main/search-panel/search-panel.component.ts
@@ -5,15 +5,10 @@ import {Component, Output, EventEmitter, OnInit} from '@angular/core';
   templateUrl: './search-panel.component.html',
   styleUrls: ['./search-panel.component.sass'],
 })
-export class SearchPanelComponent implements OnInit {
-  public inputValue:string ="";
-  @Output()titleFind = new EventEmitter<string>();
-
-  ngOnInit(): void {
-    this.inputValue = "";
-  }
-
-  filterHandler(value:string) {
-   this.titleFind.emit(value);
+export class SearchPanelComponent {
+  public inputValue: string = "";
+  @Output() titleFind = new EventEmitter<string>();
+  filterHandler(value: string) {
+    this.titleFind.emit(value);
   }
 }
