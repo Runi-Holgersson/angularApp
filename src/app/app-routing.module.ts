5c79496e349e1d6c66f5d4f4a90bf54aa1858fa2
Runi-Holgersson <runi.holger@gmail.com>
2021-10-15 13:59:48 +0400

    add authorization guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {ContentComponent} from "./content/content.component";
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {CourseRedactorComponent} from "./course-redactor/course-redactor.component";
 import {ErrorPageComponent} from "./error-page/error-page.component";
+import {AuthorizationGuard} from "./authorization.guard";
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent},
-  {path: 'courses', component: ContentComponent},
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home', component: HomePageComponent},
+  {path: 'courses', component: ContentComponent, canActivate: [AuthorizationGuard]},
   {path: 'login-page', component: LoginPageComponent},
-  {path: 'courses/:id', component: CourseRedactorComponent},
-  {path: 'courses/new', component: CourseRedactorComponent},
+  {path: 'courses/:id', component: CourseRedactorComponent, canActivate: [AuthorizationGuard]},
+  {path: 'courses/new', component: CourseRedactorComponent, canActivate: [AuthorizationGuard]},
   {path: '**', component: ErrorPageComponent}
 ];
 
---
+++ src/app/authorization.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthorizationService} from "./common/services/authorization.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationGuard implements CanActivate {
+  constructor(private router: Router, private authService: AuthorizationService) {
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(!this.authService.isAuthenticated()){
+      this.router.navigate(['/login-page']);
+      return false;
+    }
+    return true;
+  }
+}
