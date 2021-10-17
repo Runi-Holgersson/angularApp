48f1bcfc091786b39aaa0fcddffbf8947137b467
Runi-Holgersson <runi.holger@gmail.com>
2021-10-18 00:23:20 +0400

    add breadcrumps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, DoCheck} from '@angular/core';
 import {LoginPageService} from "./login-page/login-page.service";
 import {CourseRedactorService} from "./course-redactor/course-redactor.service";
 import {Router} from "@angular/router";
+import {ItemListService} from "./common/services/item-list.service";
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,11 @@ import {Router} from "@angular/router";
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements DoCheck{
+  public currentUrl: string = '';
   constructor(private router: Router) {
   }
   ngDoCheck(){
-    console.log(this.router.url, 'from app');
+    this.currentUrl = this.router.url;
   }
 }
 
--- src/app/breadcrumps/breadcrumps.component.ts
+++ src/app/breadcrumps/breadcrumps.component.ts
@@ -1,4 +1,4 @@
-import {Component, DoCheck, OnChanges, OnInit} from '@angular/core';
+import {Component, DoCheck, Input, OnChanges, OnInit} from '@angular/core';
 import {BreadCrumb} from "../common/interfaces/interfaces";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ItemListService} from "../common/services/item-list.service";
@@ -9,16 +9,35 @@ import {ItemListService} from "../common/services/item-list.service";
   templateUrl: './breadcrumps.component.html',
   styleUrls: ['./breadcrumps.component.sass']
 })
-export class BreadcrumpsComponent {
-  public breadcrumbs: BreadCrumb[] = [{path: '/courses', label: "Courses"},
-    {path: 'courses/111', label: 'Course 1'}];
+export class BreadcrumpsComponent implements OnChanges {
+  public breadcrumbs: BreadCrumb[] = [];
+  @Input() public currentUrl: string = '';
+  public breadcrumb: BreadCrumb = {path: '', label: ''};
+  public testArr:string[] = [];
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute,
               public itemListService: ItemListService) {
+    // this.currentUrl = this.itemListService.currentUrl;
   }
 
-  buildBreadcrumbs() {
-    this.activatedRoute.data.subscribe(data => console.log(data));
-    console.log(this.router.url);
+  buildBreadcrumbs(): void {
+    this.breadcrumbs = [];
+    // this.testArr = this.currentUrl.split('/');
+    // this.activatedRoute.data.subscribe(data => console.log(data));
+    this.currentUrl.split('/').map((item, index) => {
+      if(item.length) {
+        this.breadcrumb.label = item;
+        this.breadcrumb.path = this.currentUrl.split('/').slice(0, index+1).join('/');
+        console.log(this.breadcrumb);
+        this.breadcrumbs.push(Object.assign({},this.breadcrumb));
+      }
+    })
+  };
+
+  ngOnChanges() {
+    console.log(this.currentUrl, 'from breadcrumps');
+    this.buildBreadcrumbs();
+    console.log(this.breadcrumbs);
+    console.log(this.testArr);
   }
 }
