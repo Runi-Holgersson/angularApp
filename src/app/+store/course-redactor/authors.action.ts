73312e96622d9e429f4e1c56c5e13bd56b9c1653
Runi-Holgersson <runi.holger@gmail.com>
2021-11-16 14:15:38 +0400

    join type constants in enum

--- src/app/+store/course-redactor/authors.action.ts
+++ src/app/+store/course-redactor/authors.action.ts
@@ -1,24 +1,26 @@
 import { Action } from '@ngrx/store';
 import {Authors} from "../../common/interfaces/authors.interface";
 
-export const GET_AUTHORS = '[Authors] GET_AUTHORS';
-export const REMOVE_AUTHOR = '[Authors] REMOVE_AUTHOR';
-export const ADD_AUTHOR = '[Authors] ADD_AUTHOR';
+export enum AuthorsActionTypes {
+  GET_AUTHORS = '[Authors] GET_AUTHORS',
+  REMOVE_AUTHOR = '[Authors] REMOVE_AUTHOR',
+  ADD_AUTHOR = '[Authors] ADD_AUTHOR'
+}
 
 export class GetAuthors implements Action{
-  readonly type = GET_AUTHORS;
+  readonly type = AuthorsActionTypes.GET_AUTHORS;
 }
 
 // tslint:disable-next-line:max-classes-per-file
 export class RemoveAuthor implements Action{
-  readonly type = REMOVE_AUTHOR;
+  readonly type = AuthorsActionTypes.REMOVE_AUTHOR;
   constructor(public payload: Authors) {
   }
 }
 
 // tslint:disable-next-line:max-classes-per-file
 export class AddAuthor implements Action{
-  readonly type = ADD_AUTHOR;
+  readonly type = AuthorsActionTypes.ADD_AUTHOR;
   constructor(public payload: Authors) {
   }
 }
